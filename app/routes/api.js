18538538613807305a630dbfde408fb2eca7766a
Robbie Sherman <rob.w.sherman@gmail.com>
2015-07-02 18:20:53 +0100

    Comments and update

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,12 +1,10 @@
 module.exports = function(app,connection) {
 
-  app.get('/api/test', function(req, res) { //this is only a test line
-    res.send([{name:'yes'}, {name:'its'}, {name:'working'}]);
-  });
 
-// api for getting top level summary data of all sites
+  // api for getting top level summary data of all sites
   app.get('/api/pickUp', function(req,res){
     connection.query('SELECT id, name, tic_mwp from top_table', function(err, rows) {
+    // connection.query('SELECT id, mpan_export from top_table', function(err, rows) {
       if (err){
         return res.json(err);
       } else {
@@ -15,7 +13,7 @@ module.exports = function(app,connection) {
     });
   });
 
-// api for getting summary of specific site
+  // api for getting summary of specific site
   app.get('/api/displaySite/site/:id', function(req,res){
     var id = req.params.id;
     connection.query('SELECT name, id, latitude, longitude, location, tic_mwp, dnc_mw, homes_powered, carbon_saved_tones, epc from top_table where id = ?', [id], function(err, rows) {
@@ -27,7 +25,7 @@ module.exports = function(app,connection) {
     });
   });
 
-// api for getting inverter generation for specific site
+  // api for getting inverter generation for specific site
   app.get('/api/displaySite/generation/:id', function(req,res){
     var id = req.params.id;
     connection.query('SELECT UNIX_TIMESTAMP(cast(`date` AS datetime) + cast(`time` as time))*1000 as `timeU`, `generation`, `inverter_number` from `inverter_site_generation_' + id + '` order by `inverter_number`, `timeU` ;',  function(err, rows) {
@@ -39,7 +37,7 @@ module.exports = function(app,connection) {
     });
   });
 
-// get export generation
+  // get export generation
   app.get('/api/displaySite/export/:id', function(req,res){
     var id = req.params.id;
     connection.query('SELECT UNIX_TIMESTAMP(cast(`date` as datetime) + cast(`time` as time))*1000 as `timeU`, `generation` from  `export_' + id +'`;', function(err,rows){
@@ -68,6 +66,26 @@ module.exports = function(app,connection) {
     connection.query('SELECT id, dno, mpan_export from top_table where id = ' + id + ';', function(err,rows){
       if (err){
         return res.json(err);
+      } else {
+        return res.json(rows);
+      }
+    });
+  });
+  // api for getting generation of all sites
+  app.get('/api/testLoop', function(req,res){
+    var baseQuery = [];
+    var num = 1;
+    var fullQuery = "";
+    for (i=1;i<6;i++){
+      baseQuery[i]=['SELECT sum(generation) from export_'+num+';'];
+      num++;
+    }
+    for(y=1;y<baseQuery.length;y++){
+      fullQuery = fullQuery+baseQuery[y];
+    }
+    connection.query(fullQuery+'SELECT id, name, tic_mwp from top_table', function(err, rows) {
+      if (err){
+        console.log(err);
       } else {
         return res.json(rows);
       }
