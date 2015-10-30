5ccd108c0836f3de456e15ef34e71f47b7dd7bcd
Robbie Sherman <rob.w.sherman@gmail.com>
2015-10-30 17:29:08 +0000

    Calendar init

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -2,3 +2,18 @@
 $( document ).ready(function() {
   $(".button-collapse").sideNav();
 });
+
+
+function hideFlashMessages() {
+  $(this).fadeOut();
+}
+
+setTimeout(function() {
+  $('.success').each(hideFlashMessages);
+}, 5000);
+$('.success').click(hideFlashMessages);
+
+$('.datepicker').pickadate({
+  selectMonths: true, // Creates a dropdown to control month
+  selectYears: 15 // Creates a dropdown of 15 years to control year
+});
