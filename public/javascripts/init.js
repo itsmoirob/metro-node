366725f7048ebc4198b1792a7dda59030a8ce567
Robbie Sherman <rob.w.sherman@gmail.com>
2015-11-13 16:31:31 +0000

    Update init

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,6 +1,7 @@
 
-$( document ).ready(function() {
+$(document).ready(function() {
   $(".button-collapse").sideNav();
+  $('select').material_select();
 });
 
 
@@ -12,12 +13,3 @@ setTimeout(function() {
   $('.success').each(hideFlashMessages);
 }, 5000);
 $('.success').click(hideFlashMessages);
-
-$('.datepicker').pickadate({
-  selectMonths: true, // Creates a dropdown to control month
-  selectYears: 15 // Creates a dropdown of 15 years to control year
-});
-
-$(document).ready(function() {
-   $('select').material_select();
- });
