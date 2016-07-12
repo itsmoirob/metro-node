2412d825bfd6573c950e4b81b7dbbe8328e75a63
Robbie Sherman <rob.w.sherman@gmail.com>
2016-07-12 13:02:49 +0100

    Render correct jade based on login

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 // Members Page
-router.get('/', ensureAuthenticated, function(req, res, next) {
-  res.render('index', { title: 'Metro' });
+router.get('/', ensureAuthenticated, function (req, res, next) {
+	if (req.user.username == 'db') {
+		res.render('eveleyDb', { title: 'Metro' });
+	} else {
+		res.render('index', { title: 'Metro' });
+	}
 });
 
-function ensureAuthenticated(req,res,next){
-	if(req.isAuthenticated()){
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
 		return next();
 	}
 	res.redirect('/users/login');
