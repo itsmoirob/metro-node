9f7e41f9f47b9bb6b900f311f3ce521f86392477
Robbie Sherman <rob.w.sherman@gmail.com>
2016-07-21 11:20:43 +0100

    Multer is required for multiple forms

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // Handle File Uploads
-// app.use(multer({ dest: './uploads' }));
+app.use(multer({ dest: './uploads' }));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
