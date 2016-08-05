b17bcf6fe4a59db891d90bf01c9f158843f87229
Robbie Sherman <rob.w.sherman@gmail.com>
2016-08-05 14:31:52 +0100

    Remove incidents

--- routes/users.js
+++ routes/users.js
@@ -20,19 +20,19 @@ var connection = mysql.createConnection({
 // 	res.send('respond with a resource');
 // });
 
-router.get('/register', function(req, res, next) {
+router.get('/register', function (req, res, next) {
 	res.render('register', {
 		'title': 'Register'
 	});
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
 	res.render('login', {
 		'title': 'Login'
 	});
 });
 
-router.post('/register', function(req, res, next) {
+router.post('/register', function (req, res, next) {
 	// Get Form Values
 	var name = req.body.name;
 	var email = req.body.email;
@@ -70,7 +70,7 @@ router.post('/register', function(req, res, next) {
 		});
 
 		// Create User
-		User.createUser(newUser, function(err, user) {
+		User.createUser(newUser, function (err, user) {
 			if (err) throw err;
 			console.log(user);
 		});
@@ -81,26 +81,26 @@ router.post('/register', function(req, res, next) {
 	}
 });
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
 	done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-	User.getUserById(id, function(err, user) {
+passport.deserializeUser(function (id, done) {
+	User.getUserById(id, function (err, user) {
 		done(err, user);
 	});
 });
 
 passport.use(new LocalStrategy(
-	function(username, password, done) {
-		User.getUserByUsername(username, function(err, user) {
+	function (username, password, done) {
+		User.getUserByUsername(username, function (err, user) {
 			if (err) throw err;
 			if (!user) {
 				console.log('Unknown User');
 				return done(null, false, { message: 'Unknown User' });
 			}
 
-			User.comparePassword(password, user.password, function(err, isMatch) {
+			User.comparePassword(password, user.password, function (err, isMatch) {
 				if (err) throw err;
 				if (isMatch) {
 					return done(null, user);
@@ -113,152 +113,20 @@ passport.use(new LocalStrategy(
 	}
 ));
 
-router.post('/login', passport.authenticate('local', { failureRedirect: '/users/login', failureFlash: 'Invalid username or password' }), function(req, res) {
+router.post('/login', passport.authenticate('local', { failureRedirect: '/users/login', failureFlash: 'Invalid username or password' }), function (req, res) {
 	console.log('Authentication Successful');
 	req.flash('success', 'You are logged in');
 	if (req.user.username == 'db') {
 		res.redirect('/#/welcomeToEveley');
 	} else {
-		res.redirect('/'); 
+		res.redirect('/');
 	}
 });
 
-router.get('/logout', function(req, res) {
+router.get('/logout', function (req, res) {
 	req.logout();
 	req.flash('success', 'You have logged out');
 	res.redirect('/users/login');
 });
-
-router.post('/newIncidentLog', function(req, res, next) {
-	console.log("POST: ");
-	// connection.connect();
-
-	name = req.user.name;
-	var site = req.body.site;
-	var selectStartDate = req.body.selectStartDate;
-	var selectStartTime = req.body.selectStartTime;
-	var selectEndTime = null;
-	if (!req.body.selectEndTime) {
-		selectEndTime = "23:59";
-	} else {
-		selectEndTime = req.body.selectEndTime;
-	}
-	var endDateTime;
-	if (req.body.selectEndDate) {
-		endDateTime = "'" + req.body.selectEndDate + " " + selectEndTime + "'";
-	} else {
-		endDateTime = "NULL";
-	}
-	var reportedByPerson = req.body.reportedByPerson;
-	var reportedByCompany = req.body.reportedByCompany;
-	var planned = req.body.planned;
-	var lossOfGeneration = req.body.lossOfGeneration;
-	var category = req.body.category;
-	var details = req.body.details;
-	var isClosed = req.body.isClosed;
-
-
-	connection.query("start transaction; insert into incident_log values (NULL," +
-		site + ", now(), '" +
-		selectStartDate + " " + selectStartTime + "', " +
-		endDateTime + ", '" +
-		reportedByPerson + "', " +
-		reportedByCompany + ", " +
-		category + "," +
-		planned + ", " +
-		lossOfGeneration + ", '" +
-		details + "', " +
-		isClosed
-		+ ",NULL, '" + name + "', NULL, NULL); Update incident_log set incident_report_number = concat('IRF_', date(now()), '_', lpad(id-11,3,'0')) order by id desc limit 1; commit;", function(error, rows, fields) {
-			console.log(error);
-			// res.writeHead(200, {'Content-Type': 'text/plain'});
-			req.flash('success', 'Record has been inserted');
-			res.redirect('/#/incidentsAll');
-		});
-});
-
-router.post('/updateIncidentLog', function(req, res, next) {
-	console.log("POST: ");
-	// connection.connect();
-
-	name = req.user.name;
-	var log_id = req.body.log_id;
-	var selectStartDate = req.body.selectStartDate;
-	var selectStartTime = req.body.selectStartTime;
-	var startDateTime = '\'' + selectStartDate + ' ' + selectStartTime + '\'';
-	var selectEndTime = null;
-	if (!req.body.selectEndTime) {
-		selectEndTime = "23:59";
-	} else {
-		selectEndTime = req.body.selectEndTime;
-	}
-	var endDateTime;
-	if (req.body.selectEndDate) {
-		endDateTime = '\'' + req.body.selectEndDate + ' ' + selectEndTime + '\'';
-	} else {
-		endDateTime = 'NULL';
-	}
-	var reportedByPerson = req.body.reportedByPerson;
-	var reportedByCompany = req.body.reportedByCompany;
-	var planned = req.body.planned;
-	var lossOfGeneration = req.body.lossOfGeneration;
-	var category = req.body.category;
-	var details = req.body.details;
-	var status = req.body.isClosed;
-
-	//   res.send('Update incident_log set start_time = ' + startDateTime + 
-	//   ', end_time = ' + endDateTime + 
-	//   ', reported_by_person = \'' + reportedByPerson + 
-	//   '\', reported_by_company = ' + reportedByCompany +  
-	//   ', category = ' + category + 
-	//   ', planned = ' + planned + 
-	//   ', loss_of_generation = ' + lossOfGeneration + 
-	//   ', details = \'' + details + 
-	//   '\', status = ' + status + 
-	//   ', last_updated_by = \'' + name +
-	//   '\', last_updated_dateTime = now() where id = ' + log_id + ';');
-
-
-	connection.query('Update incident_log set start_time = ' + startDateTime +
-		', end_time = ' + endDateTime +
-		', reported_by_person = \'' + reportedByPerson +
-		'\', reported_by_company = ' + reportedByCompany +
-		', category = ' + category +
-		', planned = ' + planned +
-		', loss_of_generation = ' + lossOfGeneration +
-		', details = \'' + details +
-		'\', status = ' + status +
-		', last_updated_by = \'' + name +
-		'\', last_updated_dateTime = now() where id = ' + log_id + ';', function(error, rows, fields) {
-			console.log(error);
-			// res.writeHead(200, {'Content-Type': 'text/plain'});
-			req.flash('success', 'Record has been updated');
-			res.redirect('/#/report/incident?incidentId=' + log_id);
-		});
-
-});
-
-router.post('/newComment', function(req, res, next) {
-
-	comment = req.body.comment;
-	log_id = req.body.log_id;
-	name = req.user.name;
-
-	console.log('POST: ' + log_id + ' ' + comment + " " + name);
-
-	connection.query('insert into incident_comment values (null, ' + log_id + ', \'' + comment + '\', now(), \'' + name + '\');', function(error, rows, fields) {
-		console.log(error);
-		// res.writeHead(200, {'Content-Type': 'text/plain'});
-		req.flash('success', 'Comment has been inserted');
-		res.redirect('/#/report/incident?incidentId=' + log_id);
-	});
-});
-
-router.post('/closeLog', function(req, res, next) {
-	status = req.body.status;
-	console.log('POST: this is closed');
-	res.send('POST: this is closed');
-});
-
 
 module.exports = router;
