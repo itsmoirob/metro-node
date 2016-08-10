c21792cbb8414c109dae7f11a9b16ee747900a8f
Robbie Sherman <rob.w.sherman@gmail.com>
2016-08-10 17:01:13 +0100

    More formatting

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 	$(".button-collapse").sideNav({
 		// closeOnClick: true
 	});
@@ -11,18 +11,18 @@ function hideFlashMessages() {
 	$(this).fadeOut();
 }
 
-setTimeout(function() {
+setTimeout(function () {
 	$('.success').each(hideFlashMessages);
 }, 5000);
 $('.success').click(hideFlashMessages);
 
- $('.dropdown-button').dropdown({
-		inDuration: 300,
-		outDuration: 225,
-		constrain_width: false, // Does not change width of dropdown to that of the activator
-		hover: true, // Activate on hover
-		gutter: 0, // Spacing from edge
-		belowOrigin: false, // Displays dropdown below the button
-		alignment: 'left' // Displays dropdown with edge aligned to the left of button
-	}
+$('.dropdown-button').dropdown({
+	inDuration: 300,
+	outDuration: 225,
+	constrain_width: false, // Does not change width of dropdown to that of the activator
+	hover: true, // Activate on hover
+	gutter: 0, // Spacing from edge
+	belowOrigin: false, // Displays dropdown below the button
+	alignment: 'left' // Displays dropdown with edge aligned to the left of button
+}
 );
