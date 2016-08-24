cd60f3b0d058a9776b7424da117685f0037de128
Robbie Sherman <rob.w.sherman@gmail.com>
2016-08-24 10:26:09 +0100

    Add in loading

--- public/app/displayProject/displayInverter.js
+++ public/app/displayProject/displayInverter.js
@@ -7,6 +7,24 @@ angular.module('displayInverter', [
 	.controller('InverterCtrl', ['$scope', '$stateParams', '$http', '$log', '$state', 'dataFactory', function ($scope, $stateParams, $http, $log, $state, dataFactory) {
 
 		var SP = $stateParams.siteResult;
+		var transformer = 1 || $stateParams.transformer;
 		$scope.site = SP;
+		$scope.transformer = transformer;
+
+		getInverterData(SP);
+		function getInverterData(SP) {
+			dataFactory.getInverterData(SP)
+				.success(function (res) {
+					$scope.inverterData = res;
+				});
+		}
+
+		$scope.substringAng = function substringAng(word) {
+			if(word.length == 10) {
+				return word.substr(9,1);
+			} else {
+				return word.substr(9,2);
+			}
+		}
 
 	}]);
